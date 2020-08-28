84c58ed19fde5d8c6ab6663110a8bf2a36a4bb4e
Samuel K <samukatb2010@hotmail.com>
2020-08-28 00:31:41 -0300

    Switching player each turn

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,5 +1,6 @@
 package application;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -33,6 +34,25 @@ public class UI {
         System.out.flush();
     }
 
+    public static ChessPosition readChessPosition(Scanner sc) {
+        try {
+            String s = sc.nextLine();
+            char column = s.charAt(0);
+            int row = Integer.parseInt(s.substring(1));
+            return new ChessPosition(column, row);
+        }
+        catch (RuntimeException e) {
+            throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8");
+        }
+    }
+
+    public static void printMatch(ChessMatch chessMatch) {
+        printBoard(chessMatch.getPieces());
+        System.out.println();
+        System.out.println("Turn : " + chessMatch.getTurn());
+        System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+    }
+
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i=0; i<pieces.length; i++) {
             System.out.print((8 - i) + " ");
@@ -55,17 +75,6 @@ public class UI {
         System.out.println("  a b c d e f g h");
     }
 
-    public static ChessPosition readChessPosition(Scanner sc) {
-        try {
-            String s = sc.nextLine();
-            char column = s.charAt(0);
-            int row = Integer.parseInt(s.substring(1));
-            return new ChessPosition(column, row);
-        }
-        catch (RuntimeException e) {
-            throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8");
-        }
-    }
 
     private static void printPiece(ChessPiece piece, boolean background) {
         if (background) {
